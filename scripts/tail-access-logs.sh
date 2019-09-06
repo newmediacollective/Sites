2436a81f36a183d7a49ad8603475e3184efb094e
Christian Bator <christian@robinhood.com>
2019-09-06 12:44:32 -0700

    Make sure logs directory exists

--- scripts/tail-access-logs.sh
+++ scripts/tail-access-logs.sh
@@ -2,6 +2,9 @@
 
 server=$(head -n 1 config/server.txt)
 
+mkdir -p logs
+touch logs/access.log
+
 echo "Tailing access logs from $server..."
 
 ssh root@$server "tail -f /var/log/nginx/access.log" >> logs/access.log
