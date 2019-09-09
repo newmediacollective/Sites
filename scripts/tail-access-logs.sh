c7856a68b5cdf95dc289ffd7c9254ae8b9ba13ae
Christian Bator <christian@robinhood.com>
2019-09-08 21:27:30 -0700

    Better tail

--- scripts/tail-access-logs.sh
+++ scripts/tail-access-logs.sh
@@ -9,4 +9,4 @@ echo "\n---"
 echo "Tailing access logs from $server... (open $access_log_file to view)"
 
 touch $access_log_file
-ssh root@$server "tail -f $remote_access_log_file" >> $access_log_file
+ssh root@$server "tail -n0 -f $remote_access_log_file" >> $access_log_file
