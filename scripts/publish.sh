712d563dc0895557e9bad5d4599151390ff305c9
Christian Bator <bator.christian@gmail.com>
2019-09-24 16:37:53 -0700

    Update scripts

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -13,3 +13,6 @@ echo "\n---"
 echo "Publishing to $server..."
 
 rsync -vh -r --no-perms --no-owner --no-group --delete $view_dir $style_dir $image_dir $icon_dir $robots_file $sitemap_file root@$server:$remote_website_dir
+
+result=$(ssh root@$server 'chown -R www-data /website')
+echo "$result"
