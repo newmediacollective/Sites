2447fba22e07f5c69b41a2c6ad4605ab549ec7c5
Christian Bator <bator.christian@gmail.com>
2019-09-26 13:28:01 -0700

    Post local fix

--- scripts/post_local.sh
+++ scripts/post_local.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 
-token=$(pyjwt --key=$(cat app/.sites/secret.txt) encode sitename=$1)
-curl -i -X POST -H "Authorization: Bearer $token" -F "image=@$2" -F "caption=$3" http://localhost:5000/posts
-
-# curl -i -X POST -H "Sitename: $1" -F "image=@$2" -F "caption=$3" http://localhost:5000/posts
+curl -i -X POST -H "Sitename: $1" -F "image=@$2" -F "caption=$3" http://localhost:5000/posts
