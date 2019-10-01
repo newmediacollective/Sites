3297217f830571f3bcf07c8bd9be5f6bff245ce8
Christian Bator <bator.christian@gmail.com>
2019-09-30 21:55:05 -0700

    Fix secret script

--- scripts/ios_shortcut_input.py
+++ scripts/ios_shortcut_input.py
@@ -13,15 +13,15 @@ from os.path import dirname, join, realpath, isdir
 #
 scripts_dir = dirname(realpath(__file__))
 sites_dir = join(scripts_dir, "../app/sites")
-secret_path = join(sites_dir, "secret.txt")
-
-with open(secret_path, "r") as secret_file:
-    secret = secret_file.read().strip()
 
 sitenames = [sitename for sitename in os.listdir(sites_dir) if isdir(join(sites_dir, sitename))]
 
 shortcut_input = {}
 for sitename in sitenames:
+    secret_path = join(sites_dir, f"{sitename}/secret.txt")
+    with open(secret_path, "r") as secret_file:
+        secret = secret_file.read().strip()
+
     token = jwt.encode({"sitename": sitename}, secret, algorithm = "HS256").decode("utf-8")
     shortcut_input[sitename] = token
 
