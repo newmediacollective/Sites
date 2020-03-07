dfbf73348553c0ab9ac49a7c6bed2b505b3ef2ca
Ben Oztalay <boztalay@gmail.com>
2020-03-07 15:34:31 -0800

    Update the remote-posting scripts and server.conf

--- scripts/post_image.sh
+++ scripts/post_image.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 token=$(pyjwt --key=$(cat app/sites/$1/secret.txt) encode sitename=$1)
-curl -i -H "Authorization: Bearer $token" -F "post_type=image" -F "content=@$2" -F "caption=$3" -F "location=$4" https://$1/posts
+curl -i -H "Authorization: Bearer $token" -F "image=@$2" -F "caption=$3" -F "location=$4" https://$1/post_image
--- scripts/post_text.sh
+++ scripts/post_text.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 token=$(pyjwt --key=$(cat app/sites/$1/secret.txt) encode sitename=$1)
-curl -i -H "Authorization: Bearer $token" -F "post_type=text" -F "content=@$2" -F "location=$3" https://$1/posts
+curl -i -H "Authorization: Bearer $token" -F "text=@$2" -F "location=$3" https://$1/post_text
