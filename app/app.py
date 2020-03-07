32b5d508ddaac595feb09fe4c4a96446aaa7371d
Ben Oztalay <boztalay@apple.com>
2020-03-07 15:16:51 -0800

    Create separate routes for posting images and text

--- app/app.py
+++ app/app.py
@@ -37,16 +37,103 @@ def handle_401(error):
 def handle_500(error):
     return ({ "error": "internal server error" }, 500)
 
-@app.route("/posts", methods=["POST"])
-def handle_post():
+@app.route("/post_image", methods=["POST"])
+def handle_post_image():
+    # Authenticate
+    (sitename, authenticate_error) = authenticate_post_request(request)
+    if authenticate_error is not None:
+        abort(authenticate_error)
+
+    # Get the request parameters
+    if not request.form or not request.files:
+        abort(400)
+
+    image = request.files.get("image")
+    if not image:
+        abort(400)
+
+    caption = request.form.get("caption")
+    if not caption:
+        abort(400)
+
+    location = request.form.get("location")
+    if not location:
+        abort(400)
+
+    # Save image to temporary file
+    if not os.path.exists(tmp_dir):
+        os.makedirs(tmp_dir)
+
+    tmp_image_path = join(tmp_dir, image.filename)
+    image.save(tmp_image_path)
+
+    # Create post
+    try:
+        content_manager = ContentManager(app_dir = app.root_path, sitename = sitename)
+        post = content_manager.create_image_post(image_path = tmp_image_path, caption = caption, location = location)
+    except Exception as error:
+        print(error)
+        abort(500)
+
+    # Clean up temporary file
+    os.remove(tmp_image_path)
+
+    # Respond
+    return (post.to_json(), 201)
+
+@app.route("/post_text", methods=["POST"])
+def handle_post_text():
+    # Authenticate
+    (sitename, authenticate_error) = authenticate_post_request(request)
+    if authenticate_error is not None:
+        abort(authenticate_error)
+
+    # Get the request parameters
+    if not request.form or not request.files:
+        abort(400)
+
+    text = request.files.get("text")
+    if not text:
+        abort(400)
+
+    location = request.form.get("location")
+    if not location:
+        abort(400)
+
+    # Save text to temporary file
+    if not os.path.exists(tmp_dir):
+        os.makedirs(tmp_dir)
+
+    tmp_text_path = join(tmp_dir, text.filename)
+    text.save(tmp_text_path)
+
+    # Create post
+    try:
+        content_manager = ContentManager(app_dir = app.root_path, sitename = sitename)
+        post = content_manager.create_text_post(text_file_path = tmp_text_path, location = location)
+    except Exception as error:
+        print(error)
+        abort(500)
+
+    # Clean up temporary file
+    os.remove(tmp_text_path)
+
+    # Respond
+    return (post.to_json(), 201)
+
+#
+# Authentication
+#
+
+def authenticate_post_request(request):
     if app.debug:
         sitename = request.headers.get("Sitename")
         if not sitename:
-            abort(400)
+            return (None, 400)
     else:
         bearer = request.headers.get("Authorization")
         if not bearer:
-            abort(401)
+            return (None, 401)
 
         host = request.host
 
@@ -57,62 +144,19 @@ def handle_post():
             secret = secret_file.read().strip()
 
         if not secret:
-            abort(500)
+            return (None, 500)
 
         try:
             token = bearer.split()[-1]
             payload = jwt.decode(token, secret, algorithms=["HS256"])
         except:
-            abort(401)
+            return (None, 401)
 
         sitename = payload.get("sitename")
         if sitename != host:
-            abort(401)
+            return (None, 401)
 
-    if not request.form or not request.files:
-        abort(400)
-
-    post_type = request.form.get("post_type")
-    if (not post_type) or (post_type not in ["image", "text"]):
-        abort(400)
-
-    content = request.files.get("content")
-    if not content:
-        abort(400)
-
-    location = request.form.get("location")
-    if not location:
-        abort(400)
-
-    # Save content to temporary file
-    if not os.path.exists(tmp_dir):
-        os.makedirs(tmp_dir)
-
-    tmp_content_path = join(tmp_dir, content.filename)
-    content.save(tmp_content_path)
-
-    # Create post
-    content_manager = ContentManager(app_dir = app.root_path, sitename = sitename)
-
-    try:
-        if post_type == "image":
-            caption = request.form.get("caption")
-            if not caption:
-                abort(400)
-
-            post = content_manager.create_image_post(image_path = tmp_content_path, caption = caption, location = location)
-        elif post_type == "text":
-            post = content_manager.create_text_post(text_file_path = tmp_content_path, location = location)
-    except Exception as error:
-        print(error)
-        abort(500)
-
-    # Clean up temporary file
-    os.remove(tmp_content_path)
-
-    # Respond
-    return (post.to_json(), 201)
-
+    return (sitename, None)
 #
 # Main
 #
--- scripts/post_text_local.sh
+++ scripts/post_text_local.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 
-curl -i -H "Sitename: $1" -F "post_type=text" -F "content=@$2" -F "location=$3" http://localhost:5000/posts
+curl -i -H "Sitename: $1" -F "text=@$2" -F "location=$3" http://localhost:5000/post_text
