ae93bef844dfc319b242eb42af38d8279e0f6c7f
Ben Oztalay <boztalay@gmail.com>
2020-03-30 14:51:22 -0700

    Fix markdown parsing for text posts, everything was an md_header for some reason

--- app/markdown_parser.py
+++ app/markdown_parser.py
@@ -7,15 +7,14 @@ import re
 from flask import escape
 
 def parse_markdown(markdown):
+    # Replace headers
+    parsed_text = re.sub(r"^# (.+)\n", lambda x: "<p class='md_header'>" + x.group(1) + "</p>", markdown)
+
     # Replace newlines
-    parsed_text = markdown.replace("\n\n", "<br><br>")
     parsed_text = parsed_text.replace("\n", "<br>")
 
     # Embolden
     parsed_text = re.sub(r"\*\*(.+)\*\*", lambda x: "<b>" + x.group(1) + "</b>", parsed_text)
-
-    # Replace headers
-    parsed_text = re.sub(r"# (.+)", lambda x: "<p class='md_header'>" + x.group(1) + "</p>", parsed_text)
 
     # Replace urls
     parsed_text = re.sub(r"\[(.+?)\]\((.+?)\)", lambda x: "<a target='_blank' rel='noopener' href=\"" + x.group(2) + "\">" + x.group(1) + "</a>", parsed_text)
